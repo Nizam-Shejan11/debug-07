d900a935ea0c57c3595d57457a2e4b3fc90f74a8
Shejan11 <www.nijam355@gmail.com>
2022-09-15 18:08:41 +0600

    update title

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const display = document.getElementById("display");
 const question = document.getElementById("question");
-const startBtn = document.getElementById("start");
+/* ------------------------------- */
+/*            debugged:             */
+/* ------------------------------- */
+/* calling by id name was wrong: */
+
+// made it like html
+const startBtn = document.getElementById("starts");
 const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
@@ -26,7 +32,12 @@ const typeController = (e) => {
   // Handle backspace press
   if (newLetter == "Backspace") {
     userText = userText.slice(0, userText.length - 1);
-    return display.removeChild(display.lastChild);
+    /* ------------------------------- */
+    /*            debugged:             */
+    /* ------------------------------- */
+    /* Deleted the return command */
+
+    display.removeChild(display.lastChild);
   }
 
   // these are the valid character we are allowing to type
@@ -43,9 +54,19 @@ const typeController = (e) => {
   const newLetterCorrect = validate(newLetter);
 
   if (newLetterCorrect) {
-    display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
+    display.innerHTML += `<span class="green">${
+      newLetter === " " ? "▪" : newLetter
+    }</span>`;
   } else {
-    display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    //* ------------------------------- */
+    /*            debugged:             */
+    /* ------------------------------- */
+    /* If user types wrong character, We will increment error count by 1 */
+
+    errorCount++;
+    display.innerHTML += `<span class="red">${
+      newLetter === " " ? "▪" : newLetter
+    }</span>`;
   }
 
   // check if given question text is equal to user typed text
@@ -67,7 +88,13 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+
+  //* ------------------------------- */
+  /*            debugged:             */
+  /* ------------------------------- */
+  /* rounded the seconds shown in modal */
+
+  const timeTaken = Math.round((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -107,13 +134,13 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
@@ -132,8 +159,15 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
 
+  /* ------------------------------- */
+  /*            debugged:             */
+  /* ------------------------------- */
+  /* Another rounded the seconds shown in modal */
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  const timeSpent = Math.round((currentTime - startTime) / 1000);
+
+  document.getElementById("show-time").innerHTML = `${
+    startTime ? timeSpent : 0
+  } seconds`;
 }, 1000);
